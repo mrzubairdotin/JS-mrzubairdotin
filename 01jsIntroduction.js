964959e127d1b3b5913d82e2b39100f632a7d0ec
mrzubairdotin <mrzuber930@gmail.com>
2024-04-08 15:06:55 +0530

    What is JS

---
+++ 01jsIntroduction.js
@@ -0,0 +1,30 @@
+// ---------- What is JavaScript(JS) -----------
+/* 
+    JavaScript (JS) is a high-level, interpreted programming language primarily known for its use in web development. It was initially created to add interactivity to web pages in the early 1990s. Over time, JavaScript has evolved into a versatile language that can be used both on the client-side and server-side.
+    JavaScript is a powerful and versatile language that is essential for modern web development, ranging from creating interactive web pages to building complex web applications and server-side services.
+    HINDI : JavaScript (JS) एक उच्च स्तरीय, अनुप्रेषित प्रोग्रामिंग भाषा है जो मुख्य रूप से वेब विकास में उपयोग के लिए प्रसिद्ध है। यह प्रारंभिक रूप में 1990 के दशक में वेब पेजों में गतिशीलता जोड़ने के लिए बनाई गई थी। समय के साथ, JavaScript को सबसे प्रसिद्ध भाषा में बदला गया है जो क्लाइंट-साइड और सर्वर-साइड उपयोग के लिए उपयोग किया जा सकता है। JavaScript एक शक्तिशाली और बहुमुखी भाषा है जो आधुनिक वेब विकास के लिए अनिवार्य है, जिसमें इंटरैक्टिव वेब पेजों से लेकर जटिल वेब एप्लिकेशन और सर्वर-साइड सेवाओं तक का निर्माण शामिल है।
+*/
+
+// --------- Key features of JavaScript include -----------
+/*
+    -Client-side scripting
+    -Cross-platform
+    -Object-oriented
+    -Functional programming
+    -Asynchronous programming
+    -Dynamic typing
+    -Rich ecosystem
+*/ 
+
+// --------- History of JavaScript ---------------
+/* 
+  JavaScript was created by Brendan Eich in 1995 while he was working at Netscape Communications Corporation. Originally, it was developed under the name "Mocha," but it was later renamed "LiveScript" before finally settling on "JavaScript.
+  The initial purpose of JavaScript was to add interactivity to web pages. At that time, web pages were predominantly static, and there was a growing demand for more dynamic and interactive content. JavaScript provided a way to achieve this by allowing developers to manipulate the Document Object Model (DOM) of web pages, enabling actions like form validation, image slideshows, and more without relying solely on server-side processing.
+*/
+
+// --------- Javascript Versions -----------
+/*
+  JavaScript, invented by Brendan Eich, achieved the status of an ECMA standard in 1997 and adopted the official name ECMAScript. This language has evolved through several versions, namely ES1, ES2, ES3, ES5, and the transformative ES6. These updates have played a crucial role in improving and standardizing JavaScript, making it widely used and valuable in the ever-changing field of web development.
+*/
+
+
