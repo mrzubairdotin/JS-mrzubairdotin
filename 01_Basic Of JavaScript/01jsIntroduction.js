d09927a805b9a4c9009515452e73fb17ee91ffc4
mrzubairdotin <mrzuber930@gmail.com>
2024-04-09 15:55:32 +0530

    Introduction to JS

--- 01_Basic Of JavaScript/01jsIntroduction.js
+++ 01_Basic Of JavaScript/01jsIntroduction.js
@@ -26,5 +26,3 @@
 /*
   JavaScript, invented by Brendan Eich, achieved the status of an ECMA standard in 1997 and adopted the official name ECMAScript. This language has evolved through several versions, namely ES1, ES2, ES3, ES5, and the transformative ES6. These updates have played a crucial role in improving and standardizing JavaScript, making it widely used and valuable in the ever-changing field of web development.
 */
-
-
