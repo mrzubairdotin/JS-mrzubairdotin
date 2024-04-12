ed05e37e9e78fef952c42ad6aa478031b0432f32
mrzubairdotin <mrzuber930@gmail.com>
2024-04-12 14:47:06 +0530

    Switch Case in JS

---
+++ 06_Control_flow/02switchCase.js
@@ -0,0 +1,58 @@
+// switch
+//In JavaScript, the switch statement is used to perform different actions based on different conditions. It's often used as an alternative to long chains of if...else if...else statements when you have multiple possible conditions to check.
+
+/* syntax 
+switch(expression or key) {
+  case value1:
+    // Code to be executed if expression matches value1
+    break;
+  case value2:
+    // Code to be executed if expression matches value2
+    break;
+  // Additional cases as needed
+  default:
+    // Code to be executed if expression doesn't match any case
+}
+*/
+
+const month = 13;
+
+// switch (month) {
+//     case 1: 
+//         console.log("January");
+//         break;
+//     case 2: 
+//         console.log("Febuary");
+//         break;
+//     case 3: 
+//         console.log("March");
+//         break;
+//     case 4: 
+//         console.log("April");
+//         break;            
+
+//     default: 
+//         console.log("Invalid");
+//         break;
+// }
+
+const nameOfMonth = "apr"
+// switch (nameOfMonth) {
+//     case "jan": 
+//         console.log("January");
+//         break;
+//     case "feb": 
+//         console.log("Febuary");
+//         break;
+//     case "mar": 
+//         console.log("March");
+//         break;
+//     case "apr": 
+//         console.log("April");
+//         break;            
+
+//     default: 
+//         console.log("Invalid");
+//         break;
+// }
+
