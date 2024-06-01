7dcef74df8e91eb52ce84e0ce2f938ce2fda0367
mrzubairdotin <mrzuber930@gmail.com>
2024-06-01 10:00:08 +0530

    Lexical Scope in JS

---
+++ 13_Closure/01lexicalScope.js
@@ -0,0 +1,27 @@
+//  function init() {
+//         let name = "Mozilla"; 
+//         function displayName() {
+//             console.log(name); 
+//         }
+//         displayName();
+//     }
+//     init();
+
+    // function outer(){
+    //     let username = "zuair"
+    //     // console.log("OUTER", secret); Ye accsess nhi kar skta sirf outer function ke to inner funtion access kar sakte hai lekin innter suntion ke varable ya or khcuh outer acess nhi kar sakta or na hi aaps me inner funtion khch access ya share nhi kar sakte
+    //     function inner(){
+    //         let secret = "my123"
+    //         console.log("inner", username);
+    //     }
+    //     function innerTwo(){
+    //         console.log("innerTwo", username);
+    //         // console.log(secret);
+    //     }
+    //     inner()
+    //     innerTwo()
+
+    // }
+    // outer()
+
+    // console.log("TOO OUTER", username);// ye to bilkull hi bhar hai funtion se to ye bhi acess nhi kar sakta hai 
